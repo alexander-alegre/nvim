5028869b9f1dafe9d25cae1bd1f44084a7bb024e
Alexander Alegre <alexander.alegre@outlook.com>
2024-10-06 17:39:36 -0700

    wip

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -4,7 +4,7 @@ require("nvchad.configs.lspconfig").defaults()
 local lspconfig = require "lspconfig"
 
 -- EXAMPLE
-local servers = { "html", "cssls" }
+local servers = { "html", "cssls", "gopls", "htmx", "ts_ls", "tailwindcss", "emmet_ls", "templ" }
 local nvlsp = require "nvchad.configs.lspconfig"
 
 -- lsps with default config
@@ -22,3 +22,44 @@ end
 --   on_init = nvlsp.on_init,
 --   capabilities = nvlsp.capabilities,
 -- }
+
+-- configure html for templ
+lspconfig.html.setup({
+  on_attach = on_attach,
+  capabilities = nvlsp.capabilities,
+  filetypes = { "html", "templ" },
+})
+
+-- configure htmx for templ
+lspconfig.htmx.setup({
+  on_attach = on_attach,
+  capabilities = nvlsp.capabilities,
+  filetypes = { "html", "templ" },
+})
+
+-- configure css server
+lspconfig["cssls"].setup({
+  capabilities = nvlsp.capabilities,
+  on_attach = on_attach,
+})
+
+-- configure tailwindcss server
+lspconfig.tailwindcss.setup({
+  on_attach = on_attach,
+  capabilities = nvlsp.capabilities,
+  filetypes = { "templ", "astro", "javascript", "typescript", "react" },
+  settings = {
+    tailwindCSS = {
+      includeLanguages = {
+        templ = "html",
+      },
+    },
+  },
+})
+
+-- configure emmet language server
+lspconfig["emmet_ls"].setup({
+  capabilities = nvlsp.capabilities,
+  on_attach = on_attach,
+  filetypes = { "html", "typescriptreact", "javascriptreact", "css", "sass", "scss", "less", "templ" },
+})
