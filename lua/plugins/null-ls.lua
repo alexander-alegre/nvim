f3b521a14be09f4231217248a8e2137b3dfa96d7
Alexander Alegre <alexander.alegre@outlook.com>
2024-10-23 14:09:50 -0700

    fix null-ls

--- lua/plugins/null-ls.lua
+++ lua/plugins/null-ls.lua
@@ -1,9 +1,27 @@
-local null_ls = require "null-ls"
+local null_ls = require("null-ls")
+local b = null_ls.builtins
 
-local opts = {
-  sources = {
-    null_ls.builtins.formatting.prettier,
-  },
+-- Return a valid table
+return {
+  "jose-elias-alvarez/null-ls.nvim",
+  config = function()
+    null_ls.setup({
+      sources = {
+        -- Add Prettier as a formatter
+        b.formatting.prettierd.with({
+          filetypes = { "javascript", "typescript", "html", "css", "json", "yaml", "markdown", "jsx", "tsx" },
+        }),
+      },
+      on_attach = function(client, bufnr)
+        if client.supports_method("textDocument/formatting") then
+          vim.api.nvim_create_autocmd("BufWritePre", {
+            buffer = bufnr,
+            callback = function()
+              vim.lsp.buf.format({ bufnr = bufnr })
+            end,
+          })
+        end
+      end,
+    })
+  end,
 }
-
-return opts
