e1aad3e462d8c9b5e1c99a49be66a85eb27a6c01
Alexander Alegre <alexander.alegre@outlook.com>
2024-11-10 10:06:33 -0700

    update ui

--- lua/options.lua
+++ lua/options.lua
@@ -1,6 +1,11 @@
 require "nvchad.options"
 
 -- add yours here!
+local o = vim.o
+o.cursorlineopt = "both" -- to enable cursorline!
 
--- local o = vim.o
--- o.cursorlineopt ='both' -- to enable cursorline!
+vim.filetype.add {
+  extension = {
+    env = "sh", -- Treat .env files as shell scripts for syntax highlighting
+  },
+}
