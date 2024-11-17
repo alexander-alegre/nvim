8c605f3863c732f5286ec8b0b19eace5064061b4
Alexander Alegre <alexander.alegre@outlook.com>
2024-11-17 14:21:13 -0700

    enable code folding

--- lua/options.lua
+++ lua/options.lua
@@ -4,6 +4,13 @@ require "nvchad.options"
 local o = vim.o
 o.cursorlineopt = "both" -- to enable cursorline!
 
+-- folding
+o.foldmethod = "expr" -- Set fold method to "indent"
+o.foldenable = true -- Enable folding by default
+o.foldlevel = 99 -- Open all folds by default
+o.foldcolumn = "1" -- Show fold column on the side
+o.foldexpr = "nvim_treesitter#foldexpr()"
+
 vim.filetype.add {
   extension = {
     env = "sh", -- Treat .env files as shell scripts for syntax highlighting
