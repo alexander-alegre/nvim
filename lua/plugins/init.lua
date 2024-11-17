dbb223a298500ae5aec3f0a8bb2cb7788f9369a5
Alexander Alegre <alexander.alegre@outlook.com>
2024-11-17 12:55:34 -0700

    redo copilot setup

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -311,52 +311,41 @@ return {
   },
   -- copilot
   {
-    "zbirenbaum/copilot.lua",
-    cmd = "Copilot",
-    build = ":Copilot auth",
+    "github/copilot.vim",
+    event = "VeryLazy",
+    config = function()
+      -- Optional: Customize Copilot behavior here
+      vim.g.copilot_no_tab_map = true -- Disable default Tab mapping
+      vim.api.nvim_set_keymap("i", "<C-l>", 'copilot#Accept("<CR>")', { silent = true, expr = true }) -- Map <C-l> to accept suggestions
+    end,
+  },
+  {
+    "zbirenbaum/copilot-cmp",
+    dependencies = { "hrsh7th/nvim-cmp" }, -- Ensure nvim-cmp is installed
+    config = function()
+      require("copilot_cmp").setup()
+    end,
+  },
+  {
+    "hrsh7th/nvim-cmp",
+    config = function()
+      local cmp = require "cmp"
+      cmp.setup {
+        sources = {
+          { name = "copilot" }, -- Add Copilot as a completion source
+          { name = "nvim_lsp" },
+          { name = "buffer" },
+          { name = "path" },
+        },
+      }
+    end,
+  },
+  {
+    "rmagatti/auto-session",
+    lazy = false,
     opts = {
-      panel = {
-        enabled = true,
-        auto_refresh = false,
-        keymap = {
-          jump_prev = "[[",
-          jump_next = "]]",
-          accept = "<CR>",
-          refresh = "gr",
-          open = "<M-CR>",
-        },
-        layout = {
-          position = "bottom", -- | top | left | right
-          ratio = 0.4,
-        },
-      },
-      suggestion = {
-        enabled = true,
-        auto_trigger = true, -- Enable automatic triggering of suggestions
-        hide_during_completion = true,
-        debounce = 75,
-        keymap = {
-          accept = "<C-CR>",
-          accept_word = false,
-          accept_line = false,
-          next = "<M-]>",
-          prev = "<M-[>",
-          dismiss = "<C-]>",
-        },
-      },
-      filetypes = {
-        yaml = false,
-        markdown = false,
-        help = false,
-        gitcommit = false,
-        gitrebase = false,
-        hgcommit = false,
-        svn = false,
-        cvs = false,
-        ["."] = false,
-      },
-      copilot_node_command = "node", -- Node.js version must be > 18.x
-      server_opts_overrides = {},
+      suppressed_dirs = { "~/", "~/code" },
+      -- log_level = 'debug',
     },
   },
   -- PHP
