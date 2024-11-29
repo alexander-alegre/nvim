2edd43d3f1bffecbc2661f019482e0b1f8caac9c
Alexander Alegre <alexander.alegre@outlook.com>
2024-11-29 07:56:26 -0700

    add copilot next and previous

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -10,9 +10,13 @@ return {
     "github/copilot.vim",
     event = "VeryLazy",
     config = function()
-      -- Optional: Customize Copilot behavior here
       vim.g.copilot_no_tab_map = true -- Disable default Tab mapping
+      -- accept suggestion
       vim.api.nvim_set_keymap("i", "<C-l>", 'copilot#Accept("<CR>")', { silent = true, expr = true }) -- Map <C-l> to accept suggestions
+      -- next suggestion
+      vim.api.nvim_set_keymap("i", "<C-j>", "copilot#Next()", { silent = true, expr = true })
+      -- previous suggestion
+      vim.api.nvim_set_keymap("i", "<C-k>", "copilot#Previous()", { silent = true, expr = true })
     end,
   },
 }
