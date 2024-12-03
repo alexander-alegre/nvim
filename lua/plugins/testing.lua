3704b9cb00a66eab66442b64b1dbdb1e5f626f00
Alexander Alegre <alexander.alegre@outlook.com>
2024-12-03 10:50:58 -0700

    update testing file

---
+++ lua/plugins/testing.lua
@@ -0,0 +1,46 @@
+return {
+  {
+    "nvim-neotest/neotest",
+    dependencies = {
+      "nvim-neotest/nvim-nio",
+      "nvim-lua/plenary.nvim",
+      "antoinemadec/FixCursorHold.nvim",
+      "nvim-treesitter/nvim-treesitter",
+    },
+    config = function()
+      require("neotest").setup {
+        adapters = {
+          require "neotest-golang",
+          require "neotest-python",
+        },
+      }
+    end,
+  },
+  {
+    "andythigpen/nvim-coverage",
+    requires = "nvim-lua/plenary.nvim",
+    rocks = { "lua-xmlreader" },
+    config = function()
+      require("coverage").setup {
+        commands = true, -- create commands
+        highlights = {
+          -- customize highlight groups created by the plugin
+          covered = { fg = "#C3E88D" }, -- supports style, fg, bg, sp (see :h highlight-gui)
+          uncovered = { fg = "#F07178" },
+        },
+        signs = {
+          -- use your own highlight groups or text markers
+          covered = { hl = "CoverageCovered", text = "▎" },
+          uncovered = { hl = "CoverageUncovered", text = "▎" },
+        },
+        summary = {
+          -- customize the summary pop-up
+          min_coverage = 80.0, -- minimum coverage threshold (used for highlighting)
+        },
+        lang = {
+          -- customize language specific settings
+        },
+      }
+    end,
+  },
+}
