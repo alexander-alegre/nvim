a4683a4b88490eb96241febf144d0bc5649264e8
Alexander Alegre <alexander.alegre@outlook.com>
2024-12-03 13:32:11 -0700

    update coverage

--- lua/plugins/testing.lua
+++ lua/plugins/testing.lua
@@ -146,24 +146,26 @@ return {
   },
   {
     "andythigpen/nvim-coverage",
-    dependencies = { "nvim-lua/plenary.nvim" }, -- Changed from 'requires' to 'dependencies'
-    rocks = { "lua-xmlreader" },
+    event = "VeryLazy",
     config = function()
       require("coverage").setup {
         commands = true, -- create commands
         highlights = {
-          covered = { fg = "#C3E88D" },
+          -- customize highlight groups created by the plugin
+          covered = { fg = "#C3E88D" }, -- supports style, fg, bg, sp (see :h highlight-gui)
           uncovered = { fg = "#F07178" },
         },
         signs = {
+          -- use your own highlight groups or text markers
           covered = { hl = "CoverageCovered", text = "▎" },
           uncovered = { hl = "CoverageUncovered", text = "▎" },
         },
         summary = {
-          min_coverage = 80.0,
+          -- customize the summary pop-up
+          min_coverage = 80.0, -- minimum coverage threshold (used for highlighting)
         },
         lang = {
-          -- customize language-specific settings
+          -- customize language specific settings
         },
       }
     end,
