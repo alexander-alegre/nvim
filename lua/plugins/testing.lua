e99b7d4e32054b0b6519c7dac759dfdba974353e
Alexander Alegre <alexander.alegre@outlook.com>
2024-12-03 13:21:04 -0700

    update testing config

--- lua/plugins/testing.lua
+++ lua/plugins/testing.lua
@@ -7,6 +7,16 @@ return {
       "antoinemadec/FixCursorHold.nvim",
       "nvim-treesitter/nvim-treesitter",
     },
+    opts = function(_, opts)
+      opts.adapters = opts.adapters or {}
+      opts.adapters["neotest-golang"] = {
+        go_test_args = {
+          "-v",
+          "-race",
+          "-coverprofile=" .. vim.fn.getcwd() .. "/coverage.out",
+        },
+      }
+    end,
     config = function()
       require("neotest").setup {
         adapters = {
@@ -15,6 +25,85 @@ return {
         },
       }
     end,
+    keys = {
+      {
+        "<leader>ta",
+        function()
+          require("neotest").run.attach()
+        end,
+        desc = "[t]est [a]ttach",
+      },
+      {
+        "<leader>tf",
+        function()
+          require("neotest").run.run(vim.fn.expand "%")
+        end,
+        desc = "[t]est run [f]ile",
+      },
+      {
+        "<leader>tA",
+        function()
+          require("neotest").run.run(vim.uv.cwd())
+        end,
+        desc = "[t]est [A]ll files",
+      },
+      {
+        "<leader>tS",
+        function()
+          require("neotest").run.run { suite = true }
+        end,
+        desc = "[t]est [S]uite",
+      },
+      {
+        "<leader>tn",
+        function()
+          require("neotest").run.run()
+        end,
+        desc = "[t]est [n]earest",
+      },
+      {
+        "<leader>tl",
+        function()
+          require("neotest").run.run_last()
+        end,
+        desc = "[t]est [l]ast",
+      },
+      {
+        "<leader>ts",
+        function()
+          require("neotest").summary.toggle()
+        end,
+        desc = "[t]est [s]ummary",
+      },
+      {
+        "<leader>to",
+        function()
+          require("neotest").output.open { enter = true, auto_close = true }
+        end,
+        desc = "[t]est [o]utput",
+      },
+      {
+        "<leader>tO",
+        function()
+          require("neotest").output_panel.toggle()
+        end,
+        desc = "[t]est [O]utput panel",
+      },
+      {
+        "<leader>tt",
+        function()
+          require("neotest").run.stop()
+        end,
+        desc = "[t]est [t]erminate",
+      },
+      {
+        "<leader>tD",
+        function()
+          require("neotest").run.run { vim.fn.expand "%", strategy = "dap" }
+        end,
+        desc = "Debug current file",
+      },
+    },
   },
   {
     "andythigpen/nvim-coverage",
