27b7443976dc096996a85ae92eca28bb754babf3
Alexander Alegre <alexander.alegre@outlook.com>
2024-12-10 18:48:23 -0700

    add mapping for find usages of a method

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -6,5 +6,11 @@ local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
+map(
+  "n",
+  "<leader>fu",
+  ':lua require("telescope.builtin").lsp_references()<CR>',
+  { noremap = true, silent = true, desc = "Find usages" }
+)
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
